09acb7ec2639540411eb3951abfd04e9a5a5fc43
yucs <928981881@qq.com>
2015-07-02 23:41:08 +0800

    Update Median_of_Two_Sorted_Arrays.py

--- Median_of_Two_Sorted_Arrays.py
+++ Median_of_Two_Sorted_Arrays.py
@@ -1,3 +1,9 @@
+"""
+There are two sorted arrays nums1 and nums2 of size m and n respectively. 
+Find the median of the two sorted arrays.
+The overall run time complexity should be O(log (m+n)).
+"""
+ #思路：就是回归排序，但是就用循环队来保存2位 来计算即可。
 class Solution:
     # @param {integer[]} nums1
     # @param {integer[]} nums2
@@ -6,8 +12,11 @@ class Solution:
         len1=len(nums1)
         len2=len(nums2)
         mid=(len2+len1)/2+1
+        #就是标记是双还是单
         flag=(len1+len2+1)%2
+        # 报错后发现，主要轮询问题：eg: 1,3,5,7,9 如果：只有3位的话，3在que[1]上，而当5位的话，5在que[0]，。
         tag=(len1+len2)/2 %2
+        #
         que=[0,0]
         cur=cur1=cur2=0
        
