ffcdac8c3083668d950b5bf85a5dcc723a0fc051
yucs <928981881@qq.com>
2015-07-06 10:47:56 +0800

    Update Roman_to_Integer.py

--- algorithms/Roman_to_Integer.py
+++ algorithms/Roman_to_Integer.py
@@ -22,7 +22,7 @@ class Solution:
         return num
         
                 
-         #error   
+         #error   没考虑完全。
         # mp={'I':1,'II':2,'III':3,'IV':4,'V':5,
         #          'VI':6,'VII':7,'VIII':8,'IX':9,
         #          'X':10, 'XX':20, 'XXX':30,'XL':40,'L':50,
